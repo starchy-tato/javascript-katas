f0e224bd88d041fc64c5e47bc9851999da6df36d
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-06-10 18:11:16 -0600

    Update kata 4
    * add gitignore file

--- DailyKatas/Kata4.js
+++ DailyKatas/Kata4.js
@@ -2,28 +2,26 @@
 // if there are two instructors with the longest name, return the first one
 const instructorWithLongestName = function(instructors){
 //    input: an array of objects
-//   output: the object with the longest name
-
+//   output: the object in array with the longest name
 
   //   have a variable that stores the length of longest name
   let longestNameLength = 0
 //   have a variable that stores the obj name of the longest name
-  let longestName = ''
+  let objWithLongestName = null
 //   loop thru array for name in each object
-  for (const name in instructors){
-    console.log(name, instructors[name])
-    //   condition to compare each obj name length with the variable:
+  for (const object of instructors){
+    // console.log(`name in obj: ${object.name}`)
+    // condition to compare each obj name length with the variable:
+    // if there are two instructors with the longest name, return the first one
     // if obj.name.length > longestNameLength Var true replace var with new longest
-    if(instructors[name].length > longestNameLength){
-      longestNameLength = instructors[name].length
-      longestName = instructors[name]
+    if(object.name.length > longestNameLength) {
+      longestNameLength = object.name.length
+      objWithLongestName = object
     }
-    //   if false longestNameVar stays the same
-
-
-    //   return the longestNameVar
-    return longestName
+    //   if false objWithLongestName stays the same
   }
+  //   return the objWithLongestName
+  return objWithLongestName
 }
 
 console.log(instructorWithLongestName([
@@ -31,10 +29,17 @@ console.log(instructorWithLongestName([
   {name: "Jeremiah", course: 'Web'},
   {name: "Ophilia", course: 'Web'},
   {name: "Donald", course: 'Web'},
-]))
+])) // {name: "Jeremiah", course: 'Web'}
 console.log(instructorWithLongestName([
   {name: "Mathew", course: 'Web'},
   {name: "David", course: 'iOS'},
   {name: "Damascus", course: 'Web'},
-]))
+])) // {name: "Damascus", course: 'Web'}
 
+// for in - looping over properties of an object
+//  vs for of is used for an array of objects
+student = {
+  name: 'Bob',
+  age: 88,
+  gender: "male"
+}
